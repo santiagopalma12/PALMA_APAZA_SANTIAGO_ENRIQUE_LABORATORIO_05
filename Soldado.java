57ad1dff36c21038da6fbead05112c5275bbac50
DieguitoBot <diegorym10@gmail.com>
2024-10-15 09:31:25 -0600

    Por solicitud del ejercicio, se reutilizó la clase soldado de un ejercicio anterior, en caso se necesitase hacer un cambio, se hará

---
+++ Soldado.java
@@ -0,0 +1,19 @@
+package Lab_5;
+
+public class Soldado {
+    private String nombre;
+    private int nivelVida;
+
+    public Soldado(String nombre, int nivelVida) {
+        this.nombre = nombre;
+        this.nivelVida = nivelVida;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getNivelVida() {
+        return nivelVida;
+    }
+}
